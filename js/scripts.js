5a2e50c99efe44f834f7c7bbbaba687c60592c78
aldr1383 <74217841+aldr1383@users.noreply.github.com>
2020-12-11 17:43:03 -0800

    commit 18

--- js/scripts.js
+++ js/scripts.js
@@ -95,4 +95,10 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
+
 }
+
+
+
+
+
